9281671e401f687dda70f971ba9cc9198b20aa1f
Dawid Bazyluk <dawid.bazyluk@gmail.com>
2024-06-05 00:23:46 +0200

    create aplication and logic

--- src/components/Aplication.jsx
+++ src/components/Aplication.jsx
@@ -1,18 +1,61 @@
 import { Component } from "react";
+import PropTypes from "prop-types";
 
 export default class Aplication extends Component {
+  static defaultProps = {
+    step: 1,
+  };
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  
+
+  handleIncrementGood = () => {
+    this.setState((state, props) => ({
+      good: state.good + props.step,
+    }));
+  };
+  handleIncrementNeutral = () => {
+    this.setState((state, props) => ({
+      neutral: state.neutral + props.step,
+    }));
+  };
+  handleIncrementBad = () => {
+    this.setState((state, props) => ({
+      bad: state.bad + props.step,
+    }));
+  };
+
+  static propTypes = {
+    step: PropTypes.number,
+  };
+
   render() {
-      return (
-        <>
-              <h2>Please leave feedback</h2>
-              <button>Good</button>
-              <button>Neutral</button>
-              <button>Bad</button>
-              <h2>Statistics</h2>
-              <p>Good:</p>
-              <p>Neutral:</p>
-              <p>Bad:</p>
-        </>
-      );
+    const { step } = this.props;
+
+    return (
+      <>
+        <div>
+          <h2>Please leave feedback</h2>
+          <button type="button" onClick={this.handleIncrementGood}>
+            Good
+          </button>
+                <button style={{ margin: "10px" }} type="button" onClick={this.handleIncrementNeutral}>
+            Neutral
+          </button>
+          <button type="button" onClick={this.handleIncrementBad}>
+            Bad
+          </button>
+          <h2>Statistics</h2>
+          <p>Good:{this.state.good}</p>
+          <p>Neutral:{this.state.neutral}</p>
+          <p>Bad:{this.state.bad}</p>
+        </div>
+      </>
+    );
   }
 }
